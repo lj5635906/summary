fe837afb5863ab4695b739ade88f158778f077c1
jie.luo <190642964@qq.com>
2024-06-25 22:37:11 +0800

    front 创建订单

---
+++ summary-front/summary-front-web/src/main/java/com/summary/front/web/rest/order/OrderController.java
@@ -0,0 +1,37 @@
+package com.summary.front.web.rest.order;
+
+import com.summary.client.order.param.CreateOrderParam;
+import com.summary.client.remote.OrderRemoteService;
+import com.summary.common.core.dto.R;
+import jakarta.validation.Valid;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单相关接口
+ *
+ * @author jie.luo
+ * @since 2024/6/25
+ */
+@RestController
+@RequestMapping("/web/order")
+public class OrderController {
+
+    @DubboReference
+    private OrderRemoteService orderRemoteService;
+
+    /**
+     * 创建订单
+     *
+     * @param param 创建订单参数
+     * @return 订单id
+     */
+    @PostMapping("/createOrder")
+    public R<Long> createOrder(@Valid @RequestBody CreateOrderParam param) {
+        return R.success(orderRemoteService.createOrder(param));
+    }
+
+}
